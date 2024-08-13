fd360efc385c8cfe330a5f9a224e80861246786c
vishnupriyavayya <159747560+vishnupriyavayya@users.noreply.github.com>
2024-08-13 22:14:27 +0530

    Add files via upload

---
+++ mycode2.lab task 1.asm
@@ -0,0 +1,14 @@
+org 100h
+
+jmp start
+
+msg:    db      "HELLO WORLD", 0Dh,0Ah, 24h
+
+start:  mov     dx, msg
+        mov     ah, 09h 
+        int     21h 
+        
+        mov     ah, 0 
+        int     16h 
+        
+ret
